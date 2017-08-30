6627ee6e1220d4f9fbe89cc6857351ced40878f3
elonder629 <ericlonder730@gmail.com>
2017-08-30 09:02:14 -0400

    for and if

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String> sequence = new ArrayList<>();
+        sequence.add("1");
+        String temp;
+        for(int i = 0; i <= 5; i++){
+            temp = sequence.get(i);
+            for(int k = 0; k <= temp.length(); k++) {
+                if (temp.valueOf(0).equals(temp.valueOf(0))) {
+                    System.out.println("testing");
+                }
+            }
+        }
     }
 }
